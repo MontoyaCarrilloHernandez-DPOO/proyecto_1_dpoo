f6b83dac9aa4acd2c6bc4b3db8310e78d13f0757
Ma Alejandra Carrillo <ma.carrillo2@uniandes.edu.co>
2024-11-06 09:04:31 -0500

    mejores de test

--- Entrega 2/tests/pruebas/QuizTest.java
+++ Entrega 2/tests/pruebas/QuizTest.java
@@ -1,5 +1,44 @@
 package pruebas;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import learningPaths.*;
 
 public class QuizTest {
+	
+	private Quiz quiz1;
+    private Actividad prerequisito;
+    private Actividad sugerido; 
+    
+    @BeforeEach
+    void setUp( ) throws Exception
+    {
+    	
+    }
 
+    @AfterEach
+    void tearDown( ) throws Exception
+    {
+    }
+
+    @Test
+    void testGetNotaObtenida() 
+    {
+    	
+    }
+    
+    @Test
+    void testGetNotaMinima() 
+    {
+    	
+    }
+    @Test
+    void testIsExitoso() 
+    {
+    	
+    }
 }
