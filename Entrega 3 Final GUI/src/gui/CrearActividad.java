ebc2977334eff5c8131ce2dce46684e5db27c01e
Ma Alejandra Carrillo <ma.carrillo2@uniandes.edu.co>
2024-12-02 10:19:24 -0500

    cambios

--- Entrega 3 Final GUI/src/gui/CrearActividad.java
+++ Entrega 3 Final GUI/src/gui/CrearActividad.java
@@ -12,11 +12,24 @@ import usuarios.Profesor;
 import javax.swing.JTabbedPane;
 import javax.swing.JLabel;
 import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JSlider;
 
 public class CrearActividad extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textFieldCantidadPreg;
 
 	/**
 	 * Launch the application.
@@ -36,5 +49,215 @@ public class CrearActividad extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Crear una actividad: ");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(140, 0, 161, 21);
+		contentPane.add(lblNewLabel);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		tabbedPane.setBounds(0, 25, 436, 238);
+		contentPane.add(tabbedPane);
+		
+		JPanel panelQuiz = new JPanel();
+		tabbedPane.addTab("      Quiz      ", null, panelQuiz, null);
+		panelQuiz.setLayout(null);
+		
+		JButton btnCrearQuiz = new JButton("Crear Quiz");
+		btnCrearQuiz.setBounds(311, 185, 120, 23);
+		panelQuiz.add(btnCrearQuiz);
+		
+		JLabel lblTitulo = new JLabel("Titulo:");
+		lblTitulo.setBounds(10, 14, 32, 14);
+		panelQuiz.add(lblTitulo);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(94, 11, 166, 20);
+		panelQuiz.add(textField);
+		
+		JLabel lblNivel = new JLabel("Nivel:");
+		lblNivel.setBounds(10, 42, 48, 14);
+		panelQuiz.add(lblNivel);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(94, 39, 166, 20);
+		panelQuiz.add(textField_1);
+		
+		JLabel lblTiempoLmite = new JLabel("Tiempo Límite:");
+		lblTiempoLmite.setBounds(10, 70, 99, 14);
+		panelQuiz.add(lblTiempoLmite);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(94, 67, 166, 20);
+		panelQuiz.add(textField_2);
+		
+		JLabel lblObjetivo = new JLabel("Objetivo:");
+		lblObjetivo.setBounds(10, 99, 45, 14);
+		panelQuiz.add(lblObjetivo);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(94, 95, 166, 20);
+		panelQuiz.add(textField_3);
+		
+		JLabel lblCantidadDePreguntas = new JLabel("Cantidad de preguntas:");
+		lblCantidadDePreguntas.setBounds(293, 52, 128, 14);
+		panelQuiz.add(lblCantidadDePreguntas);
+		
+		JLabel lblMeta = new JLabel("Metadatos:");
+		lblMeta.setBounds(10, 239, 56, 14);
+		panelQuiz.add(lblMeta);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(94, 236, 166, 20);
+		panelQuiz.add(textField_4);
+		
+		JLabel lblPrerequisito = new JLabel("Prerequisito:");
+		lblPrerequisito.setBounds(10, 131, 80, 14);
+		panelQuiz.add(lblPrerequisito);
+		
+		JLabel lblSugerido = new JLabel("Sugerido:");
+		lblSugerido.setBounds(10, 156, 67, 14);
+		panelQuiz.add(lblSugerido);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(94, 126, 166, 20);
+		panelQuiz.add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(94, 157, 166, 20);
+		panelQuiz.add(textField_6);
+		
+		textFieldCantidadPreg = new JTextField();
+		textFieldCantidadPreg.setColumns(10);
+		textFieldCantidadPreg.setBounds(314, 77, 87, 20);
+		panelQuiz.add(textFieldCantidadPreg);
+		
+		JButton btnNewButton_1 = new JButton("Crear Preguntas");
+		btnNewButton_1.setBounds(302, 105, 111, 23);
+		panelQuiz.add(btnNewButton_1);
+		
+		//
+		JPanel panelTarea = new JPanel();
+		tabbedPane.addTab("     Tarea     ", null, panelTarea, null);
+		panelTarea.setLayout(null);
+		
+		JButton btnCrearTarea = new JButton("Crear Tarea");
+		btnCrearTarea.setBounds(311, 185, 120, 23);
+		panelTarea.add(btnCrearTarea);
+		
+		//
+		JPanel panelExamen = new JPanel();
+		tabbedPane.addTab("    Examen    ", null, panelExamen, null);
+		panelExamen.setLayout(null);
+		
+		JLabel lblTituloE = new JLabel("Titulo:");
+		lblTituloE.setBounds(10, 14, 32, 14);
+		panelExamen.add(lblTituloE);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(94, 11, 166, 20);
+		panelExamen.add(textField);
+		
+		JLabel lblNivelE = new JLabel("Nivel:");
+		lblNivelE.setBounds(10, 42, 48, 14);
+		panelExamen.add(lblNivelE);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(94, 39, 166, 20);
+		panelExamen.add(textField_1);
+		
+		JLabel lblTiempoLmiteE = new JLabel("Tiempo Límite:");
+		lblTiempoLmiteE.setBounds(10, 70, 99, 14);
+		panelExamen.add(lblTiempoLmiteE);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(94, 67, 166, 20);
+		panelExamen.add(textField_2);
+		
+		JLabel lblObjetivoE = new JLabel("Objetivo:");
+		lblObjetivoE.setBounds(10, 99, 45, 14);
+		panelExamen.add(lblObjetivoE);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(94, 95, 166, 20);
+		panelExamen.add(textField_3);
+		
+		JLabel lblCantidadDePreguntasE = new JLabel("Cantidad de preguntas:");
+		lblCantidadDePreguntasE.setBounds(293, 52, 128, 14);
+		panelExamen.add(lblCantidadDePreguntasE);
+		
+		JLabel lblMetaE = new JLabel("Metadatos:");
+		lblMeta.setBounds(10, 239, 56, 14);
+		panelExamen.add(lblMetaE);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(94, 236, 166, 20);
+		panelExamen.add(textField_4);
+		
+		JLabel lblPrerequisitoE = new JLabel("Prerequisito:");
+		lblPrerequisitoE.setBounds(10, 131, 80, 14);
+		panelExamen.add(lblPrerequisitoE);
+		
+		JLabel lblSugeridoE = new JLabel("Sugerido:");
+		lblSugeridoE.setBounds(10, 156, 67, 14);
+		panelExamen.add(lblSugeridoE);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(94, 126, 166, 20);
+		panelExamen.add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(94, 157, 166, 20);
+		panelExamen.add(textField_6);
+		
+		textFieldCantidadPreg = new JTextField();
+		textFieldCantidadPreg.setColumns(10);
+		textFieldCantidadPreg.setBounds(314, 77, 87, 20);
+		panelExamen.add(textFieldCantidadPreg);
+		
+		JButton btnCrearPreguntas = new JButton("Crear Preguntas");
+		btnCrearPreguntas.setBounds(302, 105, 111, 23);
+		panelExamen.add(btnCrearPreguntas);
+		
+		JButton btnCrearExamen = new JButton("Crear Examen");
+		btnCrearExamen.setBounds(311, 185, 120, 23);
+		panelExamen.add(btnCrearExamen);
+		
+		//
+		JPanel panelEncuesta = new JPanel();
+		tabbedPane.addTab("    Encuesta    ", null, panelEncuesta, null);
+		panelEncuesta.setLayout(null);
+		
+		JButton btnCrearEncuesta = new JButton("Crear Encuesta");
+		btnCrearEncuesta.setBounds(311, 185, 120, 23);
+		panelEncuesta.add(btnCrearEncuesta);
+		
+		
+		//
+		JPanel panelRecurso = new JPanel();
+		tabbedPane.addTab("   Recurso ", null, panelRecurso, null);
+		panelRecurso.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Crear Recurso");
+		btnNewButton.setBounds(311, 185, 120, 23);
+		panelRecurso.add(btnNewButton);
+		
+		
 	}
 }
--- Entrega 3 Final GUI/src/gui/CrearLP.java
+++ Entrega 3 Final GUI/src/gui/CrearLP.java
@@ -13,11 +13,25 @@ import javax.swing.JTabbedPane;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JTextPane;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import javax.swing.JSlider;
 
 public class CrearLP extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textFieldTitulo;
+	private JTextField textFieldDuracion;
+	private JTextField textFieldDescripcion;
+	private JTextField textFieldObjetivo;
+	private JTextField textFieldMetadatos;
 
 	/**
 	 * Create the frame.
@@ -36,13 +50,101 @@ public class CrearLP extends JFrame {
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setFont(new Font("Tahoma", Font.BOLD, 16));
 		contentPane.add(tabbedPane);
 		
-		JTextPane textPane = new JTextPane();
-		tabbedPane.addTab("New tab", null, textPane, null);
+		JPanel Crear = new JPanel();
+		tabbedPane.addTab("        Crear un Learning Path original       ", null, Crear, null);
+		Crear.setLayout(null);
 		
-		JTextPane textPane_1 = new JTextPane();
-		tabbedPane.addTab("New tab", null, textPane_1, null);
+		JLabel lblTitulo = new JLabel("Titulo:");
+		lblTitulo.setBounds(51, 31, 32, 14);
+		Crear.add(lblTitulo);
+		
+		JLabel lblDuracion = new JLabel("Duración:");
+		lblDuracion.setBounds(51, 76, 48, 14);
+		Crear.add(lblDuracion);
+		
+		JLabel lblDescripcion = new JLabel("Descripción:");
+		lblDescripcion.setBounds(51, 121, 61, 14);
+		Crear.add(lblDescripcion);
+		
+		JLabel lblObjetivo = new JLabel("Objetivo:");
+		lblObjetivo.setBounds(51, 166, 45, 14);
+		Crear.add(lblObjetivo);
+		
+		JLabel lblDificultad = new JLabel("Dificultad:");
+		lblDificultad.setBounds(51, 211, 51, 14);
+		Crear.add(lblDificultad);
+		
+		JLabel lblMeta = new JLabel("Metadatos:");
+		lblMeta.setBounds(51, 256, 56, 14);
+		Crear.add(lblMeta);
+		
+		textFieldTitulo = new JTextField();
+		textFieldTitulo.setBounds(135, 28, 166, 20);
+		Crear.add(textFieldTitulo);
+		textFieldTitulo.setColumns(10);
+		
+		textFieldDuracion = new JTextField();
+		textFieldDuracion.setColumns(10);
+		textFieldDuracion.setBounds(135, 73, 166, 20);
+		Crear.add(textFieldDuracion);
+		
+		textFieldDescripcion = new JTextField();
+		textFieldDescripcion.setColumns(10);
+		textFieldDescripcion.setBounds(135, 118, 166, 20);
+		Crear.add(textFieldDescripcion);
+		
+		textFieldObjetivo = new JTextField();
+		textFieldObjetivo.setColumns(10);
+		textFieldObjetivo.setBounds(135, 162, 166, 20);
+		Crear.add(textFieldObjetivo);
+		
+		textFieldMetadatos = new JTextField();
+		textFieldMetadatos.setColumns(10);
+		textFieldMetadatos.setBounds(135, 253, 166, 20);
+		Crear.add(textFieldMetadatos);
+		
+		JList listLP = new JList();
+		listLP.setBounds(382, 35, 266, 215);
+		Crear.add(listLP);
+		
+		JButton btnNewButton = new JButton("Crear Learning Path");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(528, 270, 144, 23);
+		Crear.add(btnNewButton);
+		
+		JLabel lblAct = new JLabel("Selecciona las actividades que tu Learning Path contendrá: ");
+		lblAct.setBounds(358, 11, 314, 14);
+		Crear.add(lblAct);
+		
+		JSlider slider = new JSlider();
+		slider.setValue(5);
+		slider.setPaintTicks(true);
+		slider.setMinimum(1);
+		slider.setMaximum(10);
+		slider.setBounds(135, 205, 166, 26);
+		Crear.add(slider);
+		
+		JPanel Duplicar = new JPanel();
+		tabbedPane.addTab("      Duplicar un Learning Path existente        ", null, Duplicar, null);
+		Duplicar.setLayout(null);
+		
+		JButton btnDuplicarLearningPath = new JButton("Duplicar Learning Path");
+		btnDuplicarLearningPath.setBounds(498, 270, 186, 23);
+		Duplicar.add(btnDuplicarLearningPath);
+		
+		JList list_1 = new JList();
+		list_1.setBounds(217, 41, 260, 218);
+		Duplicar.add(list_1);
+		
+		JLabel lblNewLabel_4 = new JLabel("Selecciona uno de los siguientes Learning Paths para duplicarlo:");
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4.setBounds(138, 16, 417, 14);
+		Duplicar.add(lblNewLabel_4);
 	}
-
 }
